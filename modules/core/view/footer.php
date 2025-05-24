d663ecc8281ab5a98421b77007c31cef1adbeed9
vladawei <gakman.v@ya.ru>
2025-05-24 23:54:49 +0300

    добавлены header, footer, index

--- modules/core/view/footer.php
+++ modules/core/view/footer.php
@@ -1,1 +1,22 @@
-Это футер
+
+
+    <div class="b001_footer flex">
+        <div class="container">
+            <div class="b001_footer_box">
+                <div class="b001_footer_logo_box">
+                    <img src="src/img/logowh.svg" alt="">
+                </div>
+                <div class="b001_footer_link_box">
+                    <a href="" class="b001_footer_link_item">
+                        ©2023 Gakman
+                    </a>
+                    <a href="" class="b001_footer_link_item">
+                        Политика конфиденциальности
+                    </a>
+                    <a href="" class="b001_footer_link_item">
+                        Разработка сайта: GAKMAN
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
--- modules/core/view/index.php
+++ modules/core/view/index.php
@@ -1,1 +1,12 @@
-Это индекс
+<div class="b003_main_screen">
+  <canvas id="myCanvas"></canvas>
+    <div class="container">
+        <div class="b003_main_screen_hero_wrapper">
+            <div class="b003_main_screen_hero">
+                <h1 class="b003_main_screen_hero_title">Ваша идея — наш код. Мы превращаем мысли в сайты.</h1>
+                <p class="b003_main_screen_hero_dicription">Создаём сайты, которые работают на ваш бизнес: быстро, стильно и без шаблонных решений. Разработка под задачу, а не «как у всех».</p>
+                <a class="b003_main_screen_hero_btn">Начать</a>
+            </div>
+        </div>
+    </div>
+</div>
