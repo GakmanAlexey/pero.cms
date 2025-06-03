934f8d53adff019f868916374bbd0ccc30d7822f
vladawei <gakman.v@ya.ru>
2025-06-03 17:10:55 +0300

    добавлена страница редактирования пользователя

---
+++ modules/user/view/admin/edit.php
@@ -0,0 +1,92 @@
+
+
+<div class="a006_header_block">
+    <div class="a006_header_title">Пользователь #1
+    </div>
+    <a href="" class="a006_add_button">
+        Сохранить
+    </a>
+</div>
+
+<div class="a006_form_box_in_user">
+        <!-- Успешно -->
+    <div class="a006_toast a006_toast_success">
+        <span class="a006_toast_icon">✔</span>
+        <span class="a006_toast_text">Данные успешно сохранены</span>
+    </div>
+
+    <!-- Ошибка -->
+    <div class="a006_toast a006_toast_error">
+        <span class="a006_toast_icon">✖</span>
+        <span class="a006_toast_text">Ошибка сохранения</span>
+    </div>
+    <form action="" method="post">
+        <div class="a006_form_box">
+            <div class="a006_input_group">
+                <div class="a006_input_wrapper">
+                    <input class="a006_input_field" type="text" name="text1" placeholder="Простой текст">
+                    <label class="a006_input_label" for="text1">Заголовок поля текста</label>
+                    <div class="a006_error_text hd">Пожалуйста, заполните это поле</div>
+                </div>
+                <div class="a006_input_wrapper">
+                    <input class="a006_input_field" type="text" name="text2" placeholder="Простой текст">
+                    <label class="a006_input_label" for="text2">Заголовок поля текста</label>
+                    <div class="a006_error_text hd">Пожалуйста, заполните это поле</div>
+                </div>
+            </div>
+            <div class="a006_input_group">
+                <div class="a006_input_wrapper a006_has_error">
+                    <input class="a006_input_field" type="text" name="text2" placeholder="Простой текст">
+                    <label class="a006_input_label" for="text2">Заголовок поля текста</label>
+                    <div class="a006_error_text">Пожалуйста, заполните это поле</div>
+                </div>
+            </div>        
+        </div>
+
+        <div class="a006_status">
+            <div class="a006_status_wrapper">
+                <div class="a006_title_in_user">
+                    Статус
+                </div>
+                <div class="a006_status_information_wrapper">
+                    <div class="a006_status_information_active ">
+                        Активный
+                    </div>
+                    <div class="a006_status_information_no_active hd">
+                        Не активный
+                    </div>
+
+                    <a href="" class="a006_status_button">
+                        Деактивировать
+                    </a>
+                    <a href="" class="a006_status_button_activate">
+                        Активировать
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="a006_ban">
+            <div class="a006_ban_wrapper">
+                <div class="a006_title_in_user">
+                    Бан
+                </div>
+
+                <div class="a006_ban_information_wrapper">
+                    <div class="a006_ban_information_no hd">
+                        Нет
+                    </div>
+                    <div class="a006_ban_information_active">
+                        Письку показал до 23.03.2026
+                    </div>
+                <a href="" class="a006_status_button_вan">
+                    Забанить
+                </a>       
+                <a href="" class="a006_status_button_unban">
+                    Разбанить
+                </a>                
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
