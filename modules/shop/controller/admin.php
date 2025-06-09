6ff36c70743efda24a0bb138aed542a157e7ac5e
GakmanAlexey <gakman@ya.ru>
2025-06-09 10:24:15 +0300

    Добавлена структура бренда

---
+++ modules/shop/controller/admin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Shop\Controller;
+
+Class Admin extends \Modules\Abs\Controller{
+//brand
+    public function index(){   
+        $this->cashe_start();
+        if($this->cache_isset) return ;
+        \Modules\Core\Modul\Head::load();
+        $this->type_show = "admin";
+        \Modules\Core\Modul\Resource::load_conf($this->type_show);
+        $this->list_file[] = APP_ROOT."/modules/shop/view/admin/brand.php";
+        $this->show();
+        $this->cashe_end();
+    }
+
+        public function new(){   
+        $this->cashe_start();
+        if($this->cache_isset) return ;
+        \Modules\Core\Modul\Head::load();
+        $this->type_show = "admin";
+        \Modules\Core\Modul\Resource::load_conf($this->type_show);
+        $this->list_file[] = APP_ROOT."/modules/shop/view/admin/newbrand.php";
+        $this->show();
+        $this->cashe_end();
+    }
+    
+}
