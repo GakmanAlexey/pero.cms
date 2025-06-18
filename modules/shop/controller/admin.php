99fe873dc9f97d85c1e63fc901e65e3691f3d22d
GakmanAlexey <gakman@ya.ru>
2025-06-18 11:32:08 +0300

    обновлен роутер и блоки у бренда

--- modules/shop/controller/admin.php
+++ modules/shop/controller/admin.php
@@ -4,7 +4,7 @@ namespace Modules\Shop\Controller;
 
 Class Admin extends \Modules\Abs\Controller{
 //brand
-    public function index(){   
+    public function brand(){   
         $this->cashe_start();
         if($this->cache_isset) return ;
         \Modules\Core\Modul\Head::load();
@@ -17,13 +17,24 @@ Class Admin extends \Modules\Abs\Controller{
         $this->cashe_end();
     }
 
-        public function new(){   
+    public function newbrand(){   
         $this->cashe_start();
         if($this->cache_isset) return ;
         \Modules\Core\Modul\Head::load();
         $this->type_show = "admin";
         \Modules\Core\Modul\Resource::load_conf($this->type_show);
         $this->list_file[] = APP_ROOT."/modules/shop/view/admin/newbrand.php";
+        $this->show();
+        $this->cashe_end();
+    }
+
+    public function editbrand(){   
+        $this->cashe_start();
+        if($this->cache_isset) return ;
+        \Modules\Core\Modul\Head::load();
+        $this->type_show = "admin";
+        \Modules\Core\Modul\Resource::load_conf($this->type_show);
+        $this->list_file[] = APP_ROOT."/modules/shop/view/admin/editbrand.php";
         $this->show();
         $this->cashe_end();
     }
