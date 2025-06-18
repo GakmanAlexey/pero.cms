54b13833b64432504467af779773e06dbdb3e4b5
vladawei <gakman.v@ya.ru>
2025-06-18 13:23:29 +0300

    добавлены: редактирование брендов, добавление брендов, страница с брендом

---
+++ modules/shop/view/admin/newbrand.php
@@ -0,0 +1,51 @@
+<form action="" method="post">
+    <div class="a016_header_block">
+        <div class="a016_header_title">Бренд #1</div>
+        <button name="save_boot" value="save" class="a016_add_button">
+            Сохранить
+        </button>
+    </div>
+    <div id="box_msg">    
+        <!-- Успешно -->
+        <div class="a016_toast a016_toast_success">
+            <span class="a016_toast_icon">✔</span>
+            <span class="a016_toast_text">Данные успешно сохранены</span>
+        </div>
+
+        <!-- Ошибка -->
+        <div class="a016_toast a016_toast_error">
+            <span class="a016_toast_icon">✖</span>
+            <span class="a016_toast_text">Ошибка сохранения</span>
+        </div>
+    </div>
+
+    <div class="a016_form_box_in_user">
+        <div class="a016_form_box">
+            <div class="a016_input_group">
+                <div class="a016_input_wrapper">
+                    <input class="a016_input_field" type="text" name="name" placeholder="Простой текст" value="">
+                    <label class="a016_input_label" for="name">Название</label>
+                    <div class="a016_error_text hd">Пожалуйста, заполните это поле</div>
+                </div>
+                <div class="a016_input_wrapper">
+                    <input class="a016_input_field" type="text" name="nomber_photo" placeholder="Простой текст" value="">
+                    <label class="a016_input_label" for="nomber_photo">Номер изображения</label>
+                    <div class="a016_error_text hd">Пожалуйста, заполните это поле</div>
+                </div>
+            </div>
+            </div>
+            <div class="a016_input_group">
+                <div class="a016_input_wrapper">
+                    <label class="a016_input_label" for="mini_discription">Краткое описание</label>
+                    <textarea class="a016_input_textarea" type="text" name="mini_discription" placeholder="Описание" value="" rows="5" cols="33"></textarea>
+                    <div class="a016_error_text hd">Пожалуйста, заполните это поле</div>
+                </div>
+                <div class="a016_input_wrapper">
+                    <label class="a016_input_label" for="discription">Описание</label>
+                    <textarea class="a016_input_textarea" type="text" name="discription" placeholder="Описание" value="" rows="5" cols="33"></textarea>
+                    <div class="a016_error_text hd">Пожалуйста, заполните это поле</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
