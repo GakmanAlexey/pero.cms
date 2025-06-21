d9b118c752777222d18472ecb67f58bb6c6e9614
vladawei <gakman.v@ya.ru>
2025-06-21 23:47:33 +0300

    добавлены категории

--- modules/shop/view/catalog_main.php
+++ modules/shop/view/catalog_main.php
@@ -1,1 +1,40 @@
-Это каталог
+
+    <div class="win flex1">
+        <div class="container">
+            <h2 class="b022_title_block">
+                Измерительный инструмент
+            </h2>   
+            <div class="b022_kategori_box">
+                <a href="#" class="b022_kategori_item">
+                    <img src="/img/image 14.png" alt="">
+                    <h4 class="b022_kategori_item_name">Пирометры</h4>
+                </a>
+
+                <a href="#" class="b022_kategori_item">
+                    <img src="/img/image-11.png" alt="">
+                    <h4 class="b022_kategori_item_name">Мультиметры</h4>
+                </a>
+
+                <a href="#" class="b022_kategori_item">
+                    <img src="/img/image-12.png" alt="">
+                    <h4 class="b022_kategori_item_name">Токовые клещи</h4>
+                </a>
+
+                <a href="#" class="b022_kategori_item">
+                    <img src="/img/image-13.png" alt="">
+                    <h4 class="b022_kategori_item_name">Тахометры</h4>
+                </a>
+
+                <a href="#" class="b022_kategori_item">
+                    <img src="/img/image-14.png" alt="">
+                    <h4 class="b022_kategori_item_name">Люксометр</h4>
+                </a>
+
+                <a href="#" class="b022_kategori_item">
+                    <img src="/img/image-15.png" alt="">
+                    <h4 class="b022_kategori_item_name">Шумометр</h4>
+                </a>
+            </div>
+
+        </div>
+    </div>
--- modules/shop/view/catalog_open.php
+++ modules/shop/view/catalog_open.php
@@ -1,1 +1,42 @@
-Это каталог открыт
+
+    <div class="win flex1">
+        <div class="container">
+            <h2 class="b023_title_block">
+                Измерительный инструмент
+            </h2>   
+            <div class="b023_kategori_box">
+                <a href="#" class="b023_kategori_item">
+                    <img src="/img/image 14.png" alt="">
+                    <h4 class="b023_kategori_item_name">Пирометры</h4>
+                </a>
+
+                <a href="#" class="b023_kategori_item">
+                    <img src="/img/image-11.png" alt="">
+                    <h4 class="b023_kategori_item_name">Мультиметры</h4>
+                </a>
+
+                <a href="#" class="b023_kategori_item">
+                    <img src="/img/image-12.png" alt="">
+                    <h4 class="b023_kategori_item_name">Токовые клещи</h4>
+                </a>
+
+                <a href="#" class="b023_kategori_item">
+                    <img src="/img/image-13.png" alt="">
+                    <h4 class="b023_kategori_item_name">Тахометры</h4>
+                </a>
+
+                <a href="#" class="b023_kategori_item">
+                    <img src="/img/image-14.png" alt="">
+                    <h4 class="b023_kategori_item_name">Люксометр</h4>
+                </a>
+
+                <a href="#" class="b023_kategori_item">
+                    <img src="/img/image-15.png" alt="">
+                    <h4 class="b023_kategori_item_name">Шумометр</h4>
+                </a>
+            </div>
+
+        </div>
+    </div>
+
+
