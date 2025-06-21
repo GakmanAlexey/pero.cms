395293bac3e9181cde0cb4e8b4c600cb794145e7
GakmanAlexey <gakman@ya.ru>
2025-06-21 23:16:46 +0300

    добавлены страницы

--- modules/shop/controller/catalog.php
+++ modules/shop/controller/catalog.php
@@ -13,4 +13,28 @@ Class Catalog extends \Modules\Abs\Controller{
         $this->show();
         $this->cashe_end();
     }
+
+    
+    public function main(){   
+        $this->cashe_start();
+        if($this->cache_isset) return ;
+        \Modules\Core\Modul\Head::load();
+        $this->type_show = "default";
+        \Modules\Core\Modul\Resource::load_conf($this->type_show);        
+        $this->list_file[] = APP_ROOT."/modules/shop/view/catalog_main.php";
+        $this->show();
+        $this->cashe_end();
+    }
+
+    
+    public function open(){   
+        $this->cashe_start();
+        if($this->cache_isset) return ;
+        \Modules\Core\Modul\Head::load();
+        $this->type_show = "default";
+        \Modules\Core\Modul\Resource::load_conf($this->type_show);        
+        $this->list_file[] = APP_ROOT."/modules/shop/view/catalog_open.php";
+        $this->show();
+        $this->cashe_end();
+    }
 }
