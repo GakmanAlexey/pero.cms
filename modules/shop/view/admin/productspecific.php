5e4c37a8b8096e995301239e10833ee078a1896c
vladawei <gakman.v@ya.ru>
2025-07-05 23:41:09 +0300

    добавлены параметры продукта и вариации

--- modules/shop/view/admin/productspecific.php
+++ modules/shop/view/admin/productspecific.php
@@ -1,1 +1,38 @@
-121
+
+<form method="post"> 
+    <div class="a032_header_block">
+        <div class="a032_header_title">Список брендов</div>
+        <a href="" class="a032_add_button">
+            Сохранить
+        </a>
+    </div>
+
+
+
+    <div class="a032_table_wrapper">
+        <div class="a032_tbody">
+            <table class="a032_table">
+                <tr class="a032_tr_body">
+                    <td class="a032_td a032_td_id">
+                        <div class="a032_input_wrapper">
+                            <select class="a032_input_field_select" name="category">
+                                <option value="" disabled selected>Выберите категорию</option>
+                                <option value="option1">Опция 1</option>
+                                <option value="option2">Опция 2</option>
+                            </select>
+                            <label class="a032_input_label" for="category">Категория</label>
+                            <div class="a032_error_text hd">Пожалуйста, выберите значение</div>
+                        </div>
+                    </td>
+                    <td class="a032_td a032_td_name">
+                        <div class="a032_input_wrapper">
+                            <input class="a032_input_field" type="text" name="nomber_photo" placeholder="Простой текст" value="">
+                            <label class="a032_input_label" for="nomber_photo">Номер изображения</label>
+                            <div class="a032_error_text hd">Пожалуйста, заполните это поле</div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</form>
