7c7bacf0b9ffb7e8ef627fa04d15b1f091092fc7
GakmanAlexey <gakman@ya.ru>
2025-07-06 10:25:07 +0300

    фикс страниц

--- modules/shop/view/admin/productspecific.php
+++ modules/shop/view/admin/productspecific.php
@@ -1,38 +1,64 @@
 
 <form method="post"> 
-    <div class="a032_header_block">
-        <div class="a032_header_title">Список брендов</div>
-        <a href="" class="a032_add_button">
+    <div class="a031_header_block">
+        <div class="a031_header_title">Список брендов</div>
+        <a href="" class="a031_add_button">
             Сохранить
         </a>
     </div>
 
 
 
-    <div class="a032_table_wrapper">
-        <div class="a032_tbody">
-            <table class="a032_table">
-                <tr class="a032_tr_body">
-                    <td class="a032_td a032_td_id">
-                        <div class="a032_input_wrapper">
-                            <select class="a032_input_field_select" name="category">
+    <div class="a031_table_wrapper">
+        <div class="a031_tbody">
+            <table class="a031_table">
+                <tr class="a031_tr_body">
+                    <td class="a031_td a031_td_id">
+                        <div class="a031_input_wrapper">
+                            <select class="a031_input_field_select" name="category">
                                 <option value="" disabled selected>Выберите категорию</option>
                                 <option value="option1">Опция 1</option>
                                 <option value="option2">Опция 2</option>
                             </select>
-                            <label class="a032_input_label" for="category">Категория</label>
-                            <div class="a032_error_text hd">Пожалуйста, выберите значение</div>
+                            <label class="a031_input_label" for="category">Категория</label>
+                            <div class="a031_error_text hd">Пожалуйста, выберите значение</div>
                         </div>
                     </td>
-                    <td class="a032_td a032_td_name">
-                        <div class="a032_input_wrapper">
-                            <input class="a032_input_field" type="text" name="nomber_photo" placeholder="Простой текст" value="">
-                            <label class="a032_input_label" for="nomber_photo">Номер изображения</label>
-                            <div class="a032_error_text hd">Пожалуйста, заполните это поле</div>
+                    <td class="a031_td a031_td_name">
+                        <div class="a031_input_wrapper">
+                            <input class="a031_input_field" type="text" name="nomber_photo" placeholder="Простой текст" value="">
+                            <label class="a031_input_label" for="nomber_photo">Номер изображения</label>
+                            <div class="a031_error_text hd">Пожалуйста, заполните это поле</div>
                         </div>
+                    </td>
+                    <td class="a031_td a031_actions_wrap">
+                    <div class="a031_actions">
+                        <a href="" class="a031_action_button" title="Удаление">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                stroke="#2F6BF2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                <!-- Корзина -->
+                                <path d="M3 6h18" />
+                                <path d="M19 6l-1 14H6L5 6" />
+                                <path d="M10 11v6" />
+                                <path d="M14 11v6" />
+                                <!-- Крышка -->
+                                <path d="M9 6V4h6v2" />
+                            </svg>
+
+                        </a>
+                    </div>
                     </td>
                 </tr>
             </table>
         </div>
     </div>
+
+    <div class="a031_btn_box_in"> 
+        <a href="" class="a031_add_button">
+            <svg class="a031_add_icon" viewBox="0 0 24 24">
+            <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"/>
+            </svg>
+            Добавить
+        </a>
+    </div>   
 </form>
